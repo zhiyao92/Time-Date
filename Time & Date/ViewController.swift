81722928ef64181798e2dc25b38a7f06d2019efa
Kelvin Tan <kelvintan@kelvins-macbook-pro.local>
2018-01-24 22:21:08 +0800

    Initial Commit

---
+++ Time & Date/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Time & Date
+//
+//  Created by Kelvin Tan on 1/24/18.
+//  Copyright © 2018 Kelvin Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
